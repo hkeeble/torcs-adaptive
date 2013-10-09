31bfcd3c9380ef7941b3d5f4bbfb00b8d830ca3f
Henri Keeble <henrikeeble@btinternet.com>
2013-10-09 15:40:20 +0100

    Initial Commit

---
+++ winTelemetry/winTelemetry.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+class winTelemetry
+{
+public:
+	winTelemetry();
+	~winTelemetry();
+	winTelemetry(const winTelemetry& param);
+	void operator=(const winTelemetry& param);
+
+private:
+};
