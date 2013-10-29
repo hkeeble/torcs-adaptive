d81790424174c66890e1fe9091a15fcc13094665
Henri Keeble <henrikeeble@btinternet.com>
2013-10-29 16:53:45 +0000

    Replaced Makefile

---
+++ winTelemetry/winTel.cpp
@@ -0,0 +1,18 @@
+#include "winTelemetry.h"
+
+winTelemetry::winTelemetry()
+{
+}
+
+winTelemetry::~winTelemetry()
+{
+}
+
+winTelemetry::winTelemetry(const winTelemetry& param)
+{
+}
+
+void winTelemetry::operator=(const winTelemetry& param)
+{
+
+}
